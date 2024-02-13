b77420415fff17ce56e36936c4410c1e65415784
Kamil Lewandowski <kamlew555@gmail.com>
2024-02-13 21:27:05 +0100

    create functions generate Form elements

--- interactive-card-details-form-main/assets/js/script.js
+++ interactive-card-details-form-main/assets/js/script.js
@@ -1,18 +1,160 @@
 
+const generateInput = (type, className, placeholder, maxLength) => {
+	const input = document.createElement("input");
+	input.setAttribute("type", type);
+	input.setAttribute("class", className);
+	input.setAttribute("placeholder", placeholder);
+	input.setAttribute("maxlength", maxLength);
+	return input;
+};
 
+const generateLabel = (attribute, text) => {
+	const label = document.createElement("label");
+	label.setAttribute("for", attribute);
+	label.innerText = text;
+	return label;
+};
+
+const generateDiv = (className) => {
+	const div = document.createElement("div");
+	div.className = className;
+	return div;
+};
+
+const generateParagraph = (className) => {
+	const paragraph = document.createElement("p");
+	paragraph.className = className;
+	return paragraph;
+};
+
+const createErrorMessage = (message, className) => {
+	const errorMessage = document.createElement("p");
+	errorMessage.innerText = message;
+	errorMessage.className = className;
+
+	return errorMessage;
+};
+const createInputElement = (type, placeholder, className) => {
+	const input = document.createElement("input");
+	input.setAttribute("type", type);
+	input.setAttribute("placeholder", placeholder);
+	input.className = className;
+
+	return input;
+};
+
+const createLabelElement = (attribute, text) => {
+	const label = document.createElement("label");
+	label.setAttribute("for", attribute);
+	label.innerText = text;
+	return label;
+};
+
+const createDivElement = (className) => {
+	const div = document.createElement("div");
+	div.className = className;
+
+	return div;
+};
 
 const renderFormElements = () => {
-    const form = document.createElement('form')
-    form.className = "card__form";
-    form.setAttribute('action', '#')
-    return form
-}
+	const form = document.createElement("form");
+	form.className = "card__form";
+	form.setAttribute("action", "#");
 
+	const cardHolderDiv = createDivElement("card__holder");
+	const cardHolderLabel = createLabelElement(
+		"CardholderName",
+		"Cardholder Name"
+	);
+	const input = createInputElement(
+		"text",
+		" e.g. Jane Appleseed",
+		"card__input"
+	);
+	const error = createErrorMessage("error", "card__error-message");
+	cardHolderDiv.appendChild(cardHolderLabel);
+	cardHolderDiv.appendChild(input);
+	cardHolderDiv.appendChild(error);
+	form.appendChild(cardHolderDiv);
+
+	const cardNumberDiv = generateDiv("card__number");
+	const cardNumberLabel = generateLabel("number", "Card Number");
+	const cardNumberInput = generateInput(
+		"text",
+		"card__input card__input--number",
+		"e.g. 1234 5678 9123 0000",
+		"19"
+	);
+	const cardNumberErrorMessage = generateParagraph("card__error-message");
+
+	cardNumberDiv.appendChild(cardNumberLabel);
+	cardNumberDiv.appendChild(cardNumberInput);
+	cardNumberDiv.appendChild(cardNumberErrorMessage);
+
+	form.appendChild(cardNumberDiv);
+
+	
+	const cardDateExpLabel = generateLabel("date", "Exp. Date (MM/YY) CVC");
+	const cardDateDiv = generateDiv("card__date");
+
+	const cardDateExpDiv = generateDiv("card__date card__date--exp");
+	const cardDateExpInput = generateInput(
+		"text",
+		"card__input card__input--month",
+		"MM",
+		"2"
+	);
+	const cardDateExpErrorMessage = generateParagraph("card__error-message");
+
+	cardDateExpDiv.appendChild(cardDateExpInput);
+	cardDateExpDiv.appendChild(cardDateExpErrorMessage);
+
+	const cardDateYearsDiv = generateDiv("card__date card__date--years");
+	const cardDateYearsInput = generateInput(
+		"text",
+		"card__input card__input--year",
+		"YY",
+		"4"
+	);
+	const cardDateYearsErrorMessage = generateParagraph("card__error-message");
+
+	cardDateYearsDiv.appendChild(cardDateYearsInput);
+	cardDateYearsDiv.appendChild(cardDateYearsErrorMessage);
+
+	const cardDateCodeDiv = generateDiv("card__date card__date--code");
+	const cardDateCodeLabel = generateLabel("", "CVC");
+	const cardDateCodeInput = generateInput(
+		"text",
+		"card__input card__input--code",
+		"e.g. 123",
+		"3"
+	);
+	const cardDateCodeErrorMessage = generateParagraph("card__error-message");
+
+	cardDateCodeDiv.appendChild(cardDateCodeLabel);
+	cardDateCodeDiv.appendChild(cardDateCodeInput);
+	cardDateCodeDiv.appendChild(cardDateCodeErrorMessage);
+
+	cardDateDiv.appendChild(cardDateExpDiv);
+	cardDateDiv.appendChild(cardDateYearsDiv);
+	cardDateDiv.appendChild(cardDateCodeDiv);
+
+	form.appendChild(cardDateExpLabel);
+	form.appendChild(cardDateDiv);
+
+	const confirmButton = document.createElement("button");
+	confirmButton.className = "card__btn";
+	confirmButton.innerText = "Confirm";
+	form.appendChild(confirmButton);
+
+	return form;
+};
 
 const createImageElement = (src, alt, className) => {
 	const img = document.createElement("img");
-	img.setAttribute('src', src);
-	img.setAttribute('alt', alt);
+	img.setAttribute("src", src);
+	img.setAttribute("alt", alt);
 	img.className = className;
 	return img;
 };
@@ -23,7 +165,6 @@ const createSpanElement = (text, className) => {
 	span.className = className;
 	return span;
 };
-
 
 const renderHeaderElements = () => {
 	const header = document.createElement("header");
@@ -86,7 +227,7 @@ const renderApp = () => {
 	const container = document.createElement("div");
 	container.className = "card__content";
 	const header = renderHeaderElements();
-    const form = renderFormElements()
+	const form = renderFormElements();
 	container.appendChild(header);
 	container.appendChild(form);
 
